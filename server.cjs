f944cb40cc95fa8c0b3ad9794cfc4eca4e209730
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2025-02-24 14:20:15 -0500

    add login via token

--- server.cjs
+++ server.cjs
@@ -1,26 +1,36 @@
 const client = require('./db/client.cjs');
 client.connect();
-const { authenticateUser } = require('./db/users.cjs');
+const { authenticateUser, logInWithToken } = require('./db/users.cjs');
 
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res, next) => {
-  res.send('WELCOME!');
-});
+app.use(express.static('dist'));
+
+// app.get('/', (req, res, next) => {
+//   res.send('WELCOME!');
+// });
 
 app.post('/api/v1/login', async(req, res, next) => {
   const { username, password } = req.body;
   try {
     const token = await authenticateUser(username, password);
-    console.log(token);
     res.send({ token: token });
   } catch(err) {
-    next(err);
+    res.send({ message: 'Bad Credentials' });
   }
 });
+
+app.get('/api/v1/login', async(req, res, next) => {
+  try {
+    const user = await logInWithToken(req.headers.authorization);
+    res.send({ user })
+  } catch(err) {
+    res.send({ message: err.message });
+  }
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`);
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,92 @@
+import { useState, useEffect } from 'react';
+
 const App = () => {
+  const [inputUsername, setInputUsername] = useState('');
+  const [inputPassword, setInputPassword] = useState('');
+
+  const [token, setToken] = useState(localStorage.getItem('token'));
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const [name, setName] = useState('');
+
+  const attemptLogInWithToken = async() => {
+    if(token) {
+      const response = await fetch('/api/v1/login', {
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `${token}`
+        }
+      });
+
+      const responseJsonObject = await response.json();
+      setName(responseJsonObject.user.username);
+    }
+  }
+
+  useEffect(() => {
+    attemptLogInWithToken();
+  }, [token]);
+
+  const logIn = async(event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('/api/v1/login', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username: inputUsername,
+          password: inputPassword
+        })
+      });
+      const tokenObj = await response.json();
+      console.log(tokenObj);
+
+      if(tokenObj.token) {
+        localStorage.setItem('token', tokenObj.token);
+        setToken(tokenObj.token);
+      } else {
+        setErrorMessage(tokenObj.message);
+      }
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  const logOut = () => {
+    localStorage.removeItem('token');
+    setToken('');
+  }
 
   return (
     <>
       <h1>ACME Auth</h1>
+
+      <h2>{errorMessage}</h2>
+
+      {
+        token ?
+          <>
+            <h1>Welcome {name}</h1> 
+            <button onClick={ logOut }>Log Out</button>
+          </> :
+          <form onSubmit={ logIn }>
+            <input 
+              placeholder="username"
+              onChange={(event) => { setInputUsername(event.target.value) }}
+            />
+
+            <input 
+              placeholder="password"
+              onChange={(event) => { setInputPassword(event.target.value) }}
+            />
+
+            <button>Log In</button>
+          </form>
+      }
+      
     </>
   )
 }
